081ea1692a8febbf70a9ee089f51422f347225e5
Natan Rauber <rauber@proton.me>
2023-04-19 14:59:43 -0300

    Update paths.dart

--- packages/utils/lib/src/paths.dart
+++ packages/utils/lib/src/paths.dart
@@ -2,5 +2,5 @@ library paths;
 
 class PATH {
   static const String tibiaDataApi = 'https://api.tibiadata.com/v3';
-  static const String forgottenLandApi = 'https://forgotten-land-api.herokuapp.com';
+  static const String forgottenLandApi = 'https://forgottenland.up.railway.app';
 }
