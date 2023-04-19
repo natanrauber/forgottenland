80227b32eecaa366bd214e1250ec94a57a005caf
Natan Rauber <rauber@proton.me>
2023-04-19 16:53:53 -0300

    Update paths.dart

--- apps/client/lib/utils/src/paths.dart
+++ apps/client/lib/utils/src/paths.dart
@@ -2,5 +2,8 @@ library paths;
 
 class PATH {
   static const String tibiaDataApi = 'https://api.tibiadata.com/v3';
-  static const String forgottenLandApi = String.fromEnvironment('API_URL');
+  static const String forgottenLandApi = String.fromEnvironment(
+    'API_URL',
+    defaultValue: 'https://forgottenland.up.railway.app',
+  );
 }
