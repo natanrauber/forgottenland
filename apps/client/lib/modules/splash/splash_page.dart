96616ee199d6295ea0a7f8eb2dd01adf99fdf9a2
Natan Rauber <rauber@proton.me>
2023-08-21 22:11:38 -0300

    Make advertising clickable with link on splash screen #82

--- apps/client/lib/modules/splash/splash_page.dart
+++ apps/client/lib/modules/splash/splash_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:forgottenland/theme/colors.dart';
 import 'package:forgottenland/utils/utils.dart';
+import 'package:forgottenland/views/widgets/src/other/app_header.dart';
 import 'package:get/get.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class SplashPage extends StatefulWidget {
   @override
@@ -8,26 +11,62 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> {
+  bool _visible = false;
+
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance.addPostFrameCallback(_runTimer);
+    WidgetsBinding.instance.addPostFrameCallback(
+      (_) async {
+        visible = true;
+        await Future<dynamic>.delayed(const Duration(seconds: 2), () => visible = false);
+        await Future<dynamic>.delayed(const Duration(seconds: 1), _pushHomeScreen);
+      },
+    );
   }
 
-  void _runTimer(_) => Future<dynamic>.delayed(const Duration(seconds: 3), _pushHomeScreen);
+  bool get visible => _visible;
+  set visible(bool value) => setState(() => _visible = value);
 
   void _pushHomeScreen() => Get.toNamed(Routes.home.name);
 
   @override
-  Widget build(BuildContext context) => Container(
-        alignment: Alignment.center,
-        margin: const EdgeInsets.all(20),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(11),
-          child: ColoredBox(
-            color: Colors.red,
-            child: Image.asset(
-              'assets/images/splash/tintas_merighi.jpeg',
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppHeader(),
+        body: Container(
+          alignment: Alignment.center,
+          padding: const EdgeInsets.all(20),
+          decoration: _backgroundDecoration,
+          child: _advertisingImage(),
+        ),
+      );
+
+  BoxDecoration get _backgroundDecoration => const BoxDecoration(
+        color: AppColors.black,
+        image: DecorationImage(
+          image: AssetImage('assets/images/background/offline.jpg'),
+          fit: BoxFit.cover,
+          alignment: Alignment.topCenter,
+          // opacity: 0.7,
+        ),
+      );
+
+  Widget _advertisingImage() => MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: GestureDetector(
+          onTap: () => launchUrlString('https://www.instagram.com/merighitintas'),
+          child: AnimatedOpacity(
+            opacity: visible ? 1.0 : 0.0,
+            duration: const Duration(seconds: 1),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(11),
+              child: Container(
+                constraints: const BoxConstraints(maxWidth: 800),
+                color: Colors.red,
+                child: Image.asset(
+                  'assets/images/splash/tintas_merighi.jpeg',
+                ),
+              ),
             ),
           ),
         ),
