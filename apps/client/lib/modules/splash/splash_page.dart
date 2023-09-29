411cc496f817e71ea0ff76ec6f43c1c4ecd6fac5
Natan Rauber <rauber@proton.me>
2023-09-29 14:03:02 -0300

    Update splash_page.dart

--- apps/client/lib/modules/splash/splash_page.dart
+++ apps/client/lib/modules/splash/splash_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:forgottenland/modules/main/controllers/main_controller.dart';
 import 'package:forgottenland/theme/colors.dart';
 import 'package:forgottenland/utils/utils.dart';
@@ -29,7 +30,7 @@ class _SplashPageState extends State<SplashPage> {
       (_) async {
         args = Get.arguments as SplashPageArguments?;
         visible = true;
-        await Future<dynamic>.delayed(const Duration(seconds: 2), () => visible = false);
+        await Future<dynamic>.delayed(const Duration(seconds: 3), () => visible = false);
         await Future<dynamic>.delayed(const Duration(seconds: 1), _redirect);
       },
     );
@@ -50,7 +51,7 @@ class _SplashPageState extends State<SplashPage> {
           alignment: Alignment.center,
           padding: const EdgeInsets.all(20),
           decoration: _backgroundDecoration,
-          child: _advertisingImage(),
+          child: _sponsor(),
         ),
       );
 
@@ -64,27 +65,72 @@ class _SplashPageState extends State<SplashPage> {
         ),
       );
 
-  Widget _advertisingImage() => MouseRegion(
-        cursor: SystemMouseCursors.click,
-        child: GestureDetector(
-          onTap: () => launchUrlString(
-            'https://www.instagram.com/merighitintas',
-            mode: LaunchMode.externalApplication,
-          ),
-          child: AnimatedOpacity(
-            opacity: visible ? 1.0 : 0.0,
-            duration: const Duration(seconds: 1),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(11),
-              child: Container(
-                constraints: const BoxConstraints(maxWidth: 800),
-                color: Colors.red,
-                child: Image.asset(
-                  'assets/images/splash/tintas_merighi.jpeg',
-                ),
-              ),
+  Widget _sponsor() => AnimatedOpacity(
+        opacity: visible ? 1.0 : 0.0,
+        duration: const Duration(seconds: 1),
+        child: MouseRegion(
+          cursor: SystemMouseCursors.click,
+          child: GestureDetector(
+            onTap: () => launchUrlString(
+              'https://www.instagram.com/merighitintas',
+              mode: LaunchMode.externalApplication,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                _sponsorText(),
+                const SizedBox(height: 20),
+                _sponsorImage(),
+                const SizedBox(height: 20),
+                _sponsorInstagramButton(),
+              ],
             ),
           ),
+        ),
+      );
+
+  Widget _sponsorText() => const Text(
+        'Sponsored by:',
+        style: TextStyle(fontSize: 18),
+      );
+
+  Widget _sponsorImage() => ClipRRect(
+        borderRadius: BorderRadius.circular(11),
+        child: Container(
+          constraints: const BoxConstraints(maxWidth: 800),
+          color: Colors.red,
+          child: Image.asset(
+            'assets/images/splash/tintas_merighi.jpeg',
+          ),
+        ),
+      );
+
+  Widget _sponsorInstagramButton() => Container(
+        width: MediaQuery.of(context).size.width - 40 > 400 ? 400 : MediaQuery.of(context).size.width - 40,
+        height: 40,
+        decoration: BoxDecoration(
+          color: AppColors.bgDefault,
+          borderRadius: BorderRadius.circular(11),
+        ),
+        alignment: Alignment.center,
+        child: const Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'View on Instagram',
+              style: TextStyle(
+                fontSize: 14,
+                height: 1,
+                fontWeight: FontWeight.w500,
+                color: AppColors.primary,
+              ),
+            ),
+            SizedBox(width: 20),
+            Icon(
+              FontAwesomeIcons.instagram,
+              color: AppColors.primary,
+            ),
+          ],
         ),
       );
 }
