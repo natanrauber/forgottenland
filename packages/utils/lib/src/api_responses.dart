c35d747b04b228d3617fbccc673fd1c23f4ae637
natan.rauber <natanrauber9911@gmail.com>
2024-01-28 11:30:12 -0300

    Fix ApiResponse

--- packages/utils/lib/src/api_responses.dart
+++ packages/utils/lib/src/api_responses.dart
@@ -3,75 +3,72 @@ import 'dart:convert';
 import 'package:shelf/shelf.dart';
 import 'package:utils/utils.dart';
 
-final Map<String, dynamic> defaultBody = <String, dynamic>{
-  "server_time": DT.germany.timeStamp(),
-  "tibia_time": DT.tibia.timeStamp(),
-};
+Map<String, Object> _responseHeader({Map<String, Object>? add}) {
+  final Map<String, Object> defaultHeader = <String, Object>{
+    "Content-Type": "application/json",
+  };
+  Map<String, Object> map = <String, Object>{};
+  map.addEntries(defaultHeader.entries);
+  if (add != null) map.addEntries(add.entries);
+  return map;
+}
+
+String _responseBody({Map<String, dynamic>? add}) {
+  final Map<String, dynamic> defaultBody = <String, dynamic>{
+    "server_time": DT.germany.timeStamp(),
+    "tibia_time": DT.tibia.timeStamp(),
+  };
+  Map<String, dynamic> map = <String, dynamic>{};
+  map.addEntries(defaultBody.entries);
+  if (add != null) map.addEntries(add.entries);
+  return jsonEncode(map.clean());
+}
 
 class ApiResponse extends Response {
   ApiResponse.success({dynamic data})
       : super(
           200,
-          headers: <String, Object>{"Content-Type": "application/json"},
-          body: jsonEncode(
-            defaultBody.map((key, value) => MapEntry(key, value))
-              ..addAll({"response": 'Success', "data": data})
-              ..clean(),
+          headers: _responseHeader(),
+          body: _responseBody(
+            add: {
+              "response": 'Success',
+              "data": data,
+            },
           ),
         );
 
   ApiResponse.accepted()
       : super(
           202,
-          headers: <String, Object>{"Content-Type": "application/json"},
-          body: jsonEncode(
-            defaultBody.map((key, value) => MapEntry(key, value))
-              ..addAll({"response": 'Accepted'})
-              ..clean(),
-          ),
+          headers: _responseHeader(),
+          body: _responseBody(add: {"response": 'Accepted'}),
         );
 
   ApiResponse.noContent()
       : super(
           204,
-          headers: <String, Object>{"Content-Type": "application/json"},
-          body: jsonEncode(
-            defaultBody.map((key, value) => MapEntry(key, value))
-              ..addAll({"response": 'No Content'})
-              ..clean(),
-          ),
+          headers: _responseHeader(),
+          body: _responseBody(add: {"response": 'No Content'}),
         );
 
   ApiResponse.notFound()
       : super(
           404,
-          headers: <String, Object>{"Content-Type": "application/json"},
-          body: jsonEncode(
-            defaultBody.map((key, value) => MapEntry(key, value))
-              ..addAll({"response": 'Not Found'})
-              ..clean(),
-          ),
+          headers: _responseHeader(),
+          body: _responseBody(add: {"response": 'Not Found'}),
         );
 
   ApiResponse.notAcceptable()
       : super(
           406,
-          headers: <String, Object>{"Content-Type": "application/json"},
-          body: jsonEncode(
-            defaultBody.map((key, value) => MapEntry(key, value))
-              ..addAll({"response": 'Not Acceptable'})
-              ..clean(),
-          ),
+          headers: _responseHeader(),
+          body: _responseBody(add: {"response": 'Not Acceptable'}),
         );
 
   ApiResponse.error(dynamic e)
       : super(
           500,
-          headers: <String, Object>{"Content-Type": "application/json"},
-          body: jsonEncode(
-            defaultBody.map((key, value) => MapEntry(key, value))
-              ..addAll({"response": e.toString()})
-              ..clean(),
-          ),
+          headers: _responseHeader(),
+          body: _responseBody(add: {"response": e.toString()}),
         );
 }
