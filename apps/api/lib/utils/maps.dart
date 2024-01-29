94c5385f14fcc4b3f8b2a0d2481a925ec9ec75e0
natan.rauber <natanrauber9911@gmail.com>
2024-01-29 16:43:28 -0300

    Fix incorrect datetimes on API

--- apps/api/lib/utils/maps.dart
+++ apps/api/lib/utils/maps.dart
@@ -1,25 +1,25 @@
 import 'package:utils/utils.dart';
 
-Map<String, String> dateToCategory = <String, String>{
-  'experiencegained+today': DT.tibia.today(),
-  'experiencegained+yesterday': DT.tibia.yesterday(),
-  'experiencegained+last7days': DT.tibia.yesterday(),
-  'experiencegained+last30days': DT.tibia.yesterday(),
-  'experiencegained+last365days': DT.tibia.yesterday(),
-  'onlinetime+today': DT.tibia.today(),
-  'onlinetime+yesterday': DT.tibia.yesterday(),
-  'onlinetime+last7days': DT.tibia.yesterday(),
-  'onlinetime+last30days': DT.tibia.yesterday(),
-};
+Map<String, String> get dateToCategory => <String, String>{
+      'experiencegained+today': DT.tibia.today(),
+      'experiencegained+yesterday': DT.tibia.yesterday(),
+      'experiencegained+last7days': DT.tibia.yesterday(),
+      'experiencegained+last30days': DT.tibia.yesterday(),
+      'experiencegained+last365days': DT.tibia.yesterday(),
+      'onlinetime+today': DT.tibia.today(),
+      'onlinetime+yesterday': DT.tibia.yesterday(),
+      'onlinetime+last7days': DT.tibia.yesterday(),
+      'onlinetime+last30days': DT.tibia.yesterday(),
+    };
 
-Map<String, String> tableToCategory = <String, String>{
-  'experiencegained+today': 'exp-gain-today',
-  'experiencegained+yesterday': 'exp-gain-last-day',
-  'experiencegained+last7days': 'exp-gain-last-7-days',
-  'experiencegained+last30days': 'exp-gain-last-30-days',
-  'experiencegained+last365days': 'exp-gain-last-365-days',
-  'onlinetime+today': 'onlinetime',
-  'onlinetime+yesterday': 'onlinetime',
-  'onlinetime+last7days': 'onlinetime-last7days',
-  'onlinetime+last30days': 'onlinetime-last30days',
-};
+Map<String, String> get tableToCategory => <String, String>{
+      'experiencegained+today': 'exp-gain-today',
+      'experiencegained+yesterday': 'exp-gain-last-day',
+      'experiencegained+last7days': 'exp-gain-last-7-days',
+      'experiencegained+last30days': 'exp-gain-last-30-days',
+      'experiencegained+last365days': 'exp-gain-last-365-days',
+      'onlinetime+today': 'onlinetime',
+      'onlinetime+yesterday': 'onlinetime',
+      'onlinetime+last7days': 'onlinetime-last7days',
+      'onlinetime+last30days': 'onlinetime-last30days',
+    };
